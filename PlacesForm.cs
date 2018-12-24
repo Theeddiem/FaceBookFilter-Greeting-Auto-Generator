d6280a2c93009c70d22763bca7fcdc0b6891c920
Eddie <eddieknaz@gmail.com>
2018-12-24 17:26:37 +0200

    k

--- PlacesForm.cs
+++ PlacesForm.cs
@@ -160,7 +160,7 @@ namespace A19Ex01EddieKnyazhinsky311354047HadasFoox205651060
 
         private void friendsListBox_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            new Thread(displayFriendCommonPlaces).Start();
+            displayFriendCommonPlaces();
         }
 
         private void popularPlacesListBox_SelectedIndexChanged(object sender, EventArgs e)
