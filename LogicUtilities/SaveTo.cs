32c41cc5cc3dbf1fd439de06c30f37082037058b
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2019-01-27 21:20:59 +0200

    asd

--- LogicUtilities/SaveTo.cs
+++ LogicUtilities/SaveTo.cs
@@ -13,29 +13,42 @@ namespace LogicUtilities
 
         public void SaveHisotry(ListBox i_History)
         {
-            string currentDir = Directory.GetCurrentDirectory();
-            string fileName = string.Format("History {0}", DateTime.Now.ToString("d-M-yyyy"));
-            string path = string.Format(@"{0}\{1}.{2}", currentDir, fileName, m_FileType);
 
-            if (File.Exists(path))
-            {
-                try
-                {
-                    File.Delete(path);
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-            }
-
-            if(i_History.Items.Count == 0)
+            if (i_History.Items.Count == 0)
             {
                 MessageBox.Show("No History to Save");
             }
 
-            saveAs(i_History,path);
+            else
+            {
+                
+                //string currentDir = Directory.GetCurrentDirectory();
+                string fileName = string.Format("History {0}", DateTime.Now.ToString("d-M-yyyy"));
+                //string path = string.Format(@"{0}\{1}.{2}", currentDir, fileName, m_FileType);
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Title = "Save As";
+                saveFileDialog.FileName = fileName;
+                saveFileDialog.Filter = string.Format("{0} file (*.{0})|*.{0}", m_FileType);
+                saveFileDialog.DefaultExt = m_FileType;
+                saveFileDialog.RestoreDirectory = true;
+                saveFileDialog.ShowDialog();
+                string path = saveFileDialog.FileName;
+
+                if (File.Exists(path))
+                {
+                    try
+                    {
+                        File.Delete(path);
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+
+                saveAs(i_History, path);
+            }
         }
 
         protected abstract void saveAs(ListBox i_History,string path);
